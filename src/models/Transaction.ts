848c5d6794a80177d91f13f6b048ef73bac7e416
Artur Paranayba <artur.paranayba@gmail.com>
2020-07-01 18:00:19 -0300

    Desafio resolvido

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,41 @@
-class Transaction {
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
+class Transactions {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
-export default Transaction;
+export default Transactions;
